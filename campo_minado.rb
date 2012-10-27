fc914f6bcbd16fa89a749f8f1a566b7c3e38d019
Willian Oizumi <oizumi.willian@gmail.com>
2012-10-26 23:31:12 -0200

    initial commit

---
+++ campo_minado.rb
@@ -0,0 +1,40 @@
+﻿class CampoMinado
+	attr_reader :matriz
+
+	def initialize(matriz)
+		@matriz = matriz
+	end
+
+	def jogada(linha, coluna)
+		return "*" if bomba?(linha, coluna)
+		contador = 0
+
+		(-1..1).each do |incremento_linha|
+			(-1..1).each do |incremento_coluna|
+				contador +=1 if bomba?(linha+incremento_linha, coluna+incremento_coluna)
+			end
+		end
+
+		contador.to_s
+	end
+
+	def bomba?(linha, coluna)
+		linha_local = @matriz[linha]
+
+		return false unless linha_local
+
+		linha_local[coluna] == "*"
+	end
+
+	def resultado
+		resultado = Array.new(@matriz.size) {|linha| linha = [] }
+
+		@matriz.each_index do |linha|
+			@matriz[linha].each_index do |coluna|
+				 resultado[linha][coluna] = jogada(linha, coluna)				 
+			end			
+		end
+		resultado
+	end
+
+end
---
+++ campo_minado_spec.rb
@@ -0,0 +1,56 @@
+﻿require_relative "campo_minado"
+
+describe "Campo Minado" do
+	it "constroi objeto com matriz" do
+		campo_minado = CampoMinado.new([["*", "."], [".", "."]])
+		campo_minado.matriz.should == [["*", "."], [".", "."]]
+	end
+
+	context "metodo bomba?" do
+		before do
+			@campo_minado = CampoMinado.new([["*", "."], [".", "."]])
+		end
+		it "posicao com bomba retorna true" do			
+			@campo_minado.bomba?(0, 0).should be_true
+		end
+		it "posicao sem bomba retorna false" do
+			@campo_minado.bomba?(0, 1).should be_false
+		end
+		it "posicao invalida retorna false" do
+			@campo_minado.bomba?(42, 5).should be_false
+		end
+	end
+
+	context "metodo jogada" do
+		before do
+			@campo_minado = CampoMinado.new([["*", ".", "."], [".", ".", "."]])
+		end
+
+		it "retorna o numero de bombas" do
+			@campo_minado.jogada(0, 1).should == "1"			
+			@campo_minado.jogada(1, 0).should == "1"
+			@campo_minado.jogada(1, 1).should == "1"
+			@campo_minado.jogada(0, 2).should == "0"
+		end
+
+		it "retorna false quando a posicao for uma bomba" do
+			@campo_minado.jogada(0, 0).should == "*"
+		end
+	end
+
+	it "metodo resultado" do
+		campo_minado = CampoMinado.new([
+			["*", ".", ".", "."], 
+			[".", ".", ".", "."], 
+			[".", "*", ".", "."], 
+			[".", ".", ".", "."]
+		])
+
+		campo_minado.resultado.should == [
+			["*", "1", "0", "0"], 
+			["2", "2", "1", "0"], 
+			["1", "*", "1", "0"], 
+			["1", "1", "1", "0"]
+		]
+	end
+end
